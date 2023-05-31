97c8fae7dde9fa77c315606bdb7816e2e1ad8b0a
Katsai-Art <sissibs1997@gmail.com>
2023-05-31 20:36:13 +0000

    Added Comments to Menu code, created branching paths

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("test")
+##print("test")
 import requests
 
 
@@ -9,32 +9,42 @@ URLdefaultpoke = "https://pokeapi.co/api/v2/pokemon-species/"
 #x = requests.get(URLprice + random.randint(a,b))
 
 ##welcome user 
-print("Welcome to the Pokedex!")
+print("Welcome to the Pokedex!\n")
 ##creating list of valid options - they can pick random, by number, or name
 valid_menu_choice = ["random", "number", "name", "by number", "by name", "1", "2", "3", "quit"]
-choice = input("You can view a random pokemon, or pick one by their name or number. \n   What would you like to do? \n [RANDOM] [BY NAME] [BY NUMBER] ")
+choice = input("You can view a random pokemon, or pick one by their name or number. \n\n   What would you like to do? \n [RANDOM] [BY NAME] [BY NUMBER] ")
 valid_choice = False
 
+##force valid input to console 
 while valid_choice == False:
+  ##if it's only text, turn it into all lowercase to make sure its a valid input
   if choice.isalpha():
     choice = choice.lower()
   if choice not in valid_menu_choice:
-    print("That's not a valid input.")
+    print("That's not a valid input.\n\n")
     choice = input("What would you like to do? \n [RANDOM] [BY NAME] [BY NUMBER] ")
     continue
   else: 
     if choice == "random" or choice == "1":
-      print("You selected random.")
-      valid_choice = True
+      choice = "random"
+      print("\nYou selected random.")
     elif choice == "name" or choice == "by name" or "2":
-      print("You selected by name.")
-      valid_choice = True
+      choice = "name"
+      print("\nYou selected by name.")
     elif choice == "number" or choice == "by number" or choice == "3":
-      print("You selected by number.")
-      valid_choice = True
+      choice = "number"
+      print("\nYou selected by number.")
     else: 
       print("Thank you for using the pokedex!")
       break
+    valid_choice = True
+
+  if choice == "random": 
+    print("do stuff here")
+  elif choice == "name":
+    print("do stuff here")
+  else: 
+    print("do stuff here")
   
     
 
