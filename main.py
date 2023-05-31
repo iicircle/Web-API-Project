620306833a4c4ad86e2baec45c8ef8f8f22bca05
Katsai-Art <sissibs1997@gmail.com>
2023-05-31 20:22:40 +0000

    Starting to add menu options

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ URLdefaultpoke = "https://pokeapi.co/api/v2/pokemon-species/"
 
 #x = requests.get(URLprice + random.randint(a,b))
 
+##welcome user 
+print("Welcome to the Pokedex!")
+##creating list of valid options - they can pick random, by number, or name
+valid_menu_choice = ["random", "number", "name"]
+choice = input("You can view a random pokemon, or pick one by their name or number. \n   What would you like to do? \n [RANDOM] [BY NAME] [BY NUMBER] ")
 print("Your random Pokémon is: ")
 
 
